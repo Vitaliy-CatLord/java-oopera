d9a57e83e41ee968a96c23bf841f0ffe2de29d42
Vitaliy <Vitaliy_Matkovskiy@mail.ru>
2025-09-26 20:52:16 +0300

    checking 1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Поехали!");
+        int i = 5;
+        System.out.println("Поехали!" + i);
+        //проверка
     }
+
 }
